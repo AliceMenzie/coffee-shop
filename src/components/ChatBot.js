a6e8f607a41fdd4eb142dbc21a782f3e755136e3
AliceMenzie <alice.menzie@gmail.com>
2021-11-05 13:40:44 +1100

    add chatbot component

---
+++ src/components/ChatBot.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ChatBot() {
+  return (
+    <div>
+      {/* <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  intent="WELCOME"
+  chat-title="espresso"
+  agent-id="d264c640-55b6-443f-92d1-70ffa357f2a8"
+  language-code="en"
+></df-messenger> */}
+    </div>
+  );
+}
