d345176f33cd431600127fac2f1cc7bc8f70124b
AliceMenzie <alice.menzie@gmail.com>
2021-11-05 13:40:09 +1100

    add navbar and chatbot components to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./components/Main";
 import Coffees from "./components/Coffees";
 import Coffee from "./components/Coffee";
+import ChatBot from "./components/ChatBot";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
     <Router>
+      <NavBar />
+      <ChatBot />
       <Switch>
         <Route exact path="/">
           <Main />
